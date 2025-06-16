cc75311ca11c644f379d8302890445cf203d4665
Himal <112574980+HimalSl@users.noreply.github.com>
2025-06-16 19:47:49 +0530

    updated the .env file and server.js
    1. Updating env
      Inside the env file defined a port
      start the server
    2. Server.js
      Created a basic server and run it in a port

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+// Load environment variables
+dotenv.config();
+
+
+// Create Express app
+const app = express();
+
+
+// Start server
+const PORT = process.env.PORT || 5000; // if the PORT is not defined in the environment, default to 5000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
